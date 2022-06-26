a07d3aa90b98b5072c9fd1ebe42f9269b9588446
zyy1939 <472038122@qq.com>
2022-06-26 17:38:27 +0800

    添加音乐网模块

---
+++ src/views/xiaotaovip/modules/XiaoTaoVipModal.vue
@@ -0,0 +1,60 @@
+<template>
+  <j-modal
+    :title="title"
+    :width="width"
+    :visible="visible"
+    switchFullscreen
+    @ok="handleOk"
+    :okButtonProps="{ class:{'jee-hidden': disableSubmit} }"
+    @cancel="handleCancel"
+    cancelText="关闭">
+    <xiao-tao-vip-form ref="realForm" @ok="submitCallback" :disabled="disableSubmit"></xiao-tao-vip-form>
+  </j-modal>
+</template>
+
+<script>
+
+  import XiaoTaoVipForm from './XiaoTaoVipForm'
+  export default {
+    name: 'XiaoTaoVipModal',
+    components: {
+      XiaoTaoVipForm
+    },
+    data () {
+      return {
+        title:'',
+        width:800,
+        visible: false,
+        disableSubmit: false
+      }
+    },
+    methods: {
+      add () {
+        this.visible=true
+        this.$nextTick(()=>{
+          this.$refs.realForm.add();
+        })
+      },
+      edit (record) {
+        this.visible=true
+        this.$nextTick(()=>{
+          this.$refs.realForm.edit(record);
+        })
+      },
+      close () {
+        this.$emit('close');
+        this.visible = false;
+      },
+      handleOk () {
+        this.$refs.realForm.submitForm();
+      },
+      submitCallback(){
+        this.$emit('ok');
+        this.visible = false;
+      },
+      handleCancel () {
+        this.close()
+      }
+    }
+  }
+</script>
